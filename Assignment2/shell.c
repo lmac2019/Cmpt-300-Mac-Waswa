aaac2adc0e6955aefb8e51cc22cbaed5bd37b10c
waswaolunga <olungaw@gmail.com>
2019-06-12 23:18:13 -0700

    Fix spacing bug

--- Assignment2/shell.c
+++ Assignment2/shell.c
@@ -87,6 +87,8 @@ bool read_and_execute_command (charPtr buff, charPtr tokens[], boolPtr in_backgr
 		exit(ERROR_CODE);
 	} else if ((length < 0) && (errno == EINTR)) {
     return true;
+  } else if ((buff[0] == '\n') || (buff[0] == '\t')) {
+    return true;
   }
 
 	// * Null terminate and strip \n.
