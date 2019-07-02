3f6aa9e9fccfbb726837062ea673817d575118ee
Mac Liu <macl@sfu.ca>
2019-07-02 14:41:43 -0700

    refactor code

--- Assignment3/stats.c
+++ Assignment3/stats.c
@@ -1,10 +1,8 @@
 #include "stats.h"
+
 int producer;
 
-
 statsStructPtr stats_array;
-
-
 
 void stats_init(int num_producers){
         producer = num_producers;
@@ -13,23 +11,25 @@ void stats_init(int num_producers){
                 stats_array[i].num_factories = i;
                 stats_array[i].candies_made = 0;
                 stats_array[i].candies_consumed = 0;
-                stats_array[i].min_delay = -1;
-                stats_array[i].max_delay = -1;
+                stats_array[i].min_delay = 0;
+                stats_array[i].max_delay = 0;
                 stats_array[i].total_delay = 0;
                 stats_array[i].avg_delay = 0;
         }
 
 }
+
 void stats_cleanup(void){
         free(stats_array);
-        stats_array = NULL;
 }
+
 void stats_record_produced(int factory_number){
         stats_array[factory_number].candies_made++;
 }
+
 void stats_record_consumed(int factory_number, double delay_in_ms){
         stats_array[factory_number].candies_consumed++;
-        if(stats_array[factory_number].min_delay == -1) {
+        if(stats_array[factory_number].total_delay == 0) {
                 stats_array[factory_number].total_delay = delay_in_ms;
                 stats_array[factory_number].min_delay = delay_in_ms;
                 stats_array[factory_number].max_delay = delay_in_ms;
@@ -46,6 +46,7 @@ void stats_record_consumed(int factory_number, double delay_in_ms){
                 stats_array[factory_number].avg_delay =   stats_array[factory_number].total_delay /stats_array[factory_number].candies_consumed;
         }
 }
+
 void stats_display(void){
         printf("%8s%15s%15s%15s%15s%15s\n","Factory#",   "#Made",  "#Eaten","Min Delay[ms]",  "Avg Delay[ms]",  "Max Delay[ms]");
         for(int i = 0; i<producer; i++) {
