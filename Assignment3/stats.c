2422a05988206dbde64b75a87cd258b648062806
Mac Liu <macl@sfu.ca>
2019-07-02 14:26:49 -0700

    changed stats to struct

--- Assignment3/stats.c
+++ Assignment3/stats.c
@@ -1,71 +1,54 @@
 #include "stats.h"
 int producer;
-int* num_factories;
-int* candies_made;
-int* candies_consumed;
-double* min_delay;
-double* max_delay;
-double* total_delay;
-double* avg_delay;
 
-// static pthread_mutex_t stats_mutex;
+
+statsStructPtr stats_array;
 
 
 
 void stats_init(int num_producers){
         producer = num_producers;
-        candies_made = malloc(producer*sizeof(int));
-        candies_consumed = malloc(producer*sizeof(int));
-        num_factories = malloc(producer*sizeof(int));
-        min_delay = malloc(producer*sizeof(double));
-        max_delay = malloc(producer*sizeof(double));
-        total_delay = malloc(producer*sizeof(double));
-        avg_delay = malloc(producer*sizeof(double));
+        stats_array = malloc(producer*sizeof(stats_t));
         for(int i = 0; i<producer; i++) {
-                candies_made[i] = 0;
-                candies_consumed[i] = 0;
-                num_factories[i] = i;
-                min_delay[i] = 0;
-                max_delay[i] = 0;
-                total_delay[i] = 0;
-                avg_delay[i] = 0;
+                stats_array[i].num_factories = i;
+                stats_array[i].candies_made = 0;
+                stats_array[i].candies_consumed = 0;
+                stats_array[i].min_delay = -1;
+                stats_array[i].max_delay = -1;
+                stats_array[i].total_delay = 0;
+                stats_array[i].avg_delay = 0;
         }
 
 }
 void stats_cleanup(void){
-        free(candies_made);
-        free(candies_consumed);
-        free(min_delay);
-        free(max_delay);
-        free(avg_delay);
-        free(total_delay);
-        free(num_factories);
+        free(stats_array);
+        stats_array = NULL;
 }
 void stats_record_produced(int factory_number){
-        candies_made[factory_number]++;
+        stats_array[factory_number].candies_made++;
 }
 void stats_record_consumed(int factory_number, double delay_in_ms){
-        candies_consumed[factory_number]++;
-        if(total_delay[factory_number] == 0){
-          total_delay[factory_number] = delay_in_ms;
-          min_delay[factory_number] = delay_in_ms;
-          avg_delay[factory_number] = delay_in_ms;
-          max_delay[factory_number] = delay_in_ms;
+        stats_array[factory_number].candies_consumed++;
+        if(stats_array[factory_number].min_delay == -1) {
+                stats_array[factory_number].total_delay = delay_in_ms;
+                stats_array[factory_number].min_delay = delay_in_ms;
+                stats_array[factory_number].max_delay = delay_in_ms;
+                stats_array[factory_number].avg_delay = delay_in_ms;
         }
         else{
-          total_delay[factory_number] += delay_in_ms;
-          if(min_delay[factory_number] > delay_in_ms){
-            min_delay[factory_number] = delay_in_ms;
-          }
-          if(max_delay[factory_number]< delay_in_ms){
-            max_delay[factory_number] = delay_in_ms;
-          }
-          avg_delay[factory_number] = total_delay[factory_number]/candies_consumed[factory_number];
+                stats_array[factory_number].total_delay += delay_in_ms;
+                if(stats_array[factory_number].min_delay > delay_in_ms) {
+                        stats_array[factory_number].min_delay = delay_in_ms;
+                }
+                if(stats_array[factory_number].max_delay< delay_in_ms) {
+                        stats_array[factory_number].max_delay = delay_in_ms;
+                }
+                stats_array[factory_number].avg_delay =   stats_array[factory_number].total_delay /stats_array[factory_number].candies_consumed;
         }
 }
 void stats_display(void){
         printf("%8s%15s%15s%15s%15s%15s\n","Factory#",   "#Made",  "#Eaten","Min Delay[ms]",  "Avg Delay[ms]",  "Max Delay[ms]");
         for(int i = 0; i<producer; i++) {
-                printf("%8d%15d%15d%15.5f%15.5f%15.5f\n",num_factories[i],candies_made[i],candies_consumed[i],min_delay[i],avg_delay[i],max_delay[i]);
+                printf("%8d%15d%15d%15.5f%15.5f%15.5f\n",stats_array[i].num_factories,stats_array[i].candies_made,stats_array[i].candies_consumed,stats_array[i].min_delay,stats_array[i].avg_delay,stats_array[i].max_delay);
         }
 }
--- Assignment3/stats.h
+++ Assignment3/stats.h
@@ -3,6 +3,18 @@
 #include "helpers.h"
 #include "macros.h"
 
+typedef struct stats{
+  int num_factories;
+  int candies_made;
+  int candies_consumed;
+  double min_delay;
+  double max_delay;
+  double total_delay;
+  double avg_delay;
+} stats_t;
+
+#define statsStructPtr stats_t*
+
 
 void stats_init(int num_producers);
 void stats_cleanup(void);
