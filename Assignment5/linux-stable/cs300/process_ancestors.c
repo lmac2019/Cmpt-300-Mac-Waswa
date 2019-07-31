56c9fe5985625dc2b37c8266dfb4e0487a236769
Mac Liu <macl@sfu.ca>
2019-07-31 06:56:42 -0700

    added basic process_anscestor initailizer

---
+++ Assignment5/linux-stable/cs300/process_ancestors.c
@@ -0,0 +1,27 @@
+#include <linux/kernel.h>
+#include <linux/errno.h>
+#include <linux/uaccess.h>
+#include "process_ancestors.h"
+
+#define voidPtr void*
+
+asmlinkage long sys_process_ancestors(
+  struct process_info info_array[],
+  long size,
+  long *num_filled
+){
+
+  struct process_info process;
+  struct task_struct *task;
+  struct list_head *list;
+
+  if (size <= 0) {
+    printk("error - invalid argument: size must be <= 0\n");
+    return -EINVAL;
+  }
+
+  printk("size: %ld\n",size);
+
+
+
+ }
