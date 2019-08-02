69088e5e044b17b6f13062fac71f71d0c2289978
Mac Liu <macl@sfu.ca>
2019-08-01 17:03:59 -0700

    copy and print whole process info

--- Assignment5/linux-stable/cs300/process_ancestors.c
+++ Assignment5/linux-stable/cs300/process_ancestors.c
@@ -2,6 +2,7 @@
 #include <linux/errno.h>
 #include <linux/uaccess.h>
 #include <linux/sched.h>
+#include <linux/cred.h>
 #include "process_ancestors.h"
 
 
@@ -13,8 +14,9 @@ asmlinkage long sys_process_ancestors(
 
   struct process_info process;
   struct task_struct *cur_task;
-  // struct list_head *list;
-  int i = 0;
+  struct list_head *head;
+  long children_num = 0;
+  long sibling_num = 0;
 
   if (size <= 0) {
     printk("error - invalid argument: size must be <= 0\n");
@@ -26,8 +28,26 @@ asmlinkage long sys_process_ancestors(
     printk("current pid: %ld\n",process.pid);
     strcpy(process.name,cur_task->comm);
     printk("current pid: %s\n",process.name);
+    process.uid = cur_task->cred->uid.val;
+    printk("current uid: %ld\n",process.uid);
+    process.state = cur_task->state;
+    printk("current state: %ld\n",process.state);
+    process.nvcsw = cur_task->nvcsw;
+    printk("current nvcsw: %ld\n",process.nvcsw);
+    process.nivcsw = cur_task->nivcsw;
+    printk("current nivcsw: %ld\n",process.state);
+    list_for_each(head,&cur_task->children){
+      children_num++;
+    }
+    list_for_each(head,&cur_task->sibling){
+      sibling_num++;
+    }
+
+    process.num_children = children_num;
+    printk("current num_children: %ld\n",process.num_children);
+    process.num_siblings = sibling_num;
+    printk("current num_siblings: %ld\n",process.num_siblings);
     cur_task = cur_task->parent;
-    i++;
   }
 
   printk("size: %ld\n",size);
