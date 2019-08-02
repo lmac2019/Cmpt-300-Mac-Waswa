174664ebbb251c332cf9a5fe82d648f7c67b77c4
Mac Liu <macl@sfu.ca>
2019-08-01 19:24:41 -0700

    added test but have ramdom number or address issue or bug

--- Assignment5/test-syscall/process_ancestors_testsyscall.c
+++ Assignment5/test-syscall/process_ancestors_testsyscall.c
@@ -20,5 +20,16 @@ void test_process_ancestors_syscall(void){
 
   int result = syscall(_PROCESS_ANCESTORS_, process, size, num);
 
+  for(int i = 0; i< size;i++){
+    printf("pid: %ld\n",process[i].pid );
+    printf("name: %s\n",process[i].name );
+    printf("state: %ld\n",process[i].state );
+    printf("uid: %ld\n",process[i].uid );
+    printf("nvscw: %ld\n",process[i].nvcsw );
+    printf("nivcsw: %ld\n",process[i].nivcsw );
+    printf("num_children: %ld\n",process[i].num_children );
+    printf("num_siblings: %ld\n",process[i].num_siblings );
+  }
+
   printf("..Rising to user level w/ result = %d\n", result);
 }
