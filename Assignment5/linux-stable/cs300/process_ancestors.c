5c6c479185b3b5f9d7b6b7ae120a97d9489634e8
Mac Liu <macl@sfu.ca>
2019-08-04 09:04:12 -0700

    remove printk in kernel

--- Assignment5/linux-stable/cs300/process_ancestors.c
+++ Assignment5/linux-stable/cs300/process_ancestors.c
@@ -18,8 +18,6 @@ asmlinkage long sys_process_ancestors(
   long sibling_num = 0;
   long i;
 
-  printk("size: %ld\n", size);
-
   if (size <= 0) {
     printk("error - invalid argument: size must be > 0\n");
     return -EINVAL;
@@ -29,37 +27,21 @@ asmlinkage long sys_process_ancestors(
 
   for (i = 0; cur_task != cur_task->parent; i++, cur_task = cur_task->parent) {
     process.pid = cur_task->pid;
-    printk("current pid: %ld\n", process.pid);
-
     strcpy(process.name,cur_task->comm);
-    printk("current name: %s\n", process.name);
-
     process.uid = (long)(cur_task->cred->uid.val);
-    printk("current uid: %ld\n", process.uid);
-
     process.state = cur_task->state;
-    printk("current state: %ld\n", process.state);
-
     process.nvcsw = cur_task->nvcsw;
-    printk("current nvcsw: %ld\n", process.nvcsw);
-
     process.nivcsw = cur_task->nivcsw;
-    printk("current nivcsw: %ld\n", process.nivcsw);
-
     list_for_each(head, &cur_task->children) {
       children_num++;
     }
 
     process.num_children = children_num;
-    printk("current num_children: %ld\n", process.num_children);
-
     list_for_each(head, &cur_task->sibling) {
       sibling_num++;
     }
 
     process.num_siblings = sibling_num;
-    printk("current num_siblings: %ld\n", process.num_siblings);
-
 
     if (__copy_to_user(&info_array[i], &process, sizeof(process))) {
       printk("error - bad address in info array\n");
@@ -67,7 +49,6 @@ asmlinkage long sys_process_ancestors(
     }
   }
 
-  printk("i: %ld\n", i);
   if (__copy_to_user(num_filled, &i, sizeof(i))) {
     printk("error - bad address in num_filled\n");
     return -EFAULT;
